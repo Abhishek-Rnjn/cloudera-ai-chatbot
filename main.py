bc2ff217a6ae8ce96d50c2bad543c1c95951043e
Abhishek <abhishek.ranjan@cloudera.com>
2024-09-16 18:25:53 +0530

    Basic LLM calling endpoint

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from typing import Union
 from fastapi import FastAPI
-
+from Services.api_caller import predict
 app = FastAPI()
 
 
 @app.get("/")
 def health_check():
     return {"Health": "okay"}
+
+
+@app.get("/chat")
+def chat_with_model(input: str):
+    return predict(input)
---
+++ utils/CONSTS.py
@@ -0,0 +1,5 @@
+CHAT_ENDPOINT=("https://ml-cb4a4d8b-dea.env-hack.svbr-nqvp.int.cldr.work/namespaces/serving-default/endpoints/llama-3"
+               "-1-70b/v1/chat/completions")
+EMBEDDING_ENDPOINT=("https://ml-cb4a4d8b-dea.env-hack.svbr-nqvp.int.cldr.work/namespaces/serving-default/endpoints"
+                    "/snowflake-a100/v1/embeddings")
+
